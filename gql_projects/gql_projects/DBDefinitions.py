029c652ca900ed7545bb20750822aafa6d63b228
Spana21 <spaniklukas@seznam.cz>
2023-11-11 18:45:58 +0100

    DBDefinitions comments

--- gql_projects/gql_projects/DBDefinitions.py
+++ gql_projects/gql_projects/DBDefinitions.py
@@ -54,125 +54,149 @@ def UUIDFKey(*, ForeignKey=None, nullable=False):
 
 
 class ProjectModel(BaseModel):
+    """
+    Represents a project in the system.
+    """
     __tablename__ = "projects"
 
     id = UUIDColumn()
 
-    name = Column(String)
-    startdate = Column(DateTime)
-    enddate = Column(DateTime)
+    name = Column(String, comment="Name of the project")
+    startdate = Column(DateTime, comment="Start date of the project")
+    enddate = Column(DateTime, comment="End date of the project")
 
-    projecttype_id = Column(ForeignKey("projecttypes.id"), index=True)
+    projecttype_id = Column(ForeignKey("projecttypes.id"), index=True, comment="Foreign key referencing the project type")
     projecttype = relationship("ProjectTypeModel", back_populates="projects")
 
     group_id = UUIDFKey(nullable=True)#Column(ForeignKey("groups.id"), index=True)
     #group = relationship("groupModel")
 
-    created = Column(DateTime, server_default=sqlalchemy.sql.func.now())
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    created = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp when the project was created")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the project")
     createdby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
     changedby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
 
 
 class ProjectTypeModel(BaseModel):
+    """
+    Represents a type of project in the system.
+    """
     __tablename__ = "projecttypes"
 
     id = UUIDColumn()
-    name = Column(String)
-    name_en = Column(String)
+    name = Column(String, comment="Name of the project type")
+    name_en = Column(String, comment="English name of the project type")
 
-    category_id = Column(ForeignKey("projectcategories.id"), index=True, nullable=True)
+    category_id = Column(ForeignKey("projectcategories.id"), index=True, nullable=True, comment="Foreign key referencing the project category")
     projects = relationship("ProjectModel", back_populates="projecttype")
 
-    created = Column(DateTime, server_default=sqlalchemy.sql.func.now())
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    created = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp when the project type was created")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the project type")
     createdby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
     changedby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
 
 
 class ProjectCategoryModel(BaseModel):
+    """
+    Represents a category for projects in the system.
+    """
     __tablename__ = "projectcategories"
 
     id = UUIDColumn()
-    name = Column(String)
-    name_en = Column(String)
+    name = Column(String, comment="Name of the project category")
+    name_en = Column(String, comment="English name of the project category")
 
-    created = Column(DateTime, server_default=sqlalchemy.sql.func.now())
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    created = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp when the project category was created")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the project category")
     createdby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
     changedby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
 
 class FinanceModel(BaseModel):
+    """
+    Represents financial information related to projects in the system.
+    """
     __tablename__ = "projectfinances"
 
     id = UUIDColumn()
-    name = Column(String)
-    amount = Column(sqlalchemy.types.DECIMAL(precision=13, scale=3))
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    name = Column(String, comment="Name of the financial information")
+    amount = Column(sqlalchemy.types.DECIMAL(precision=13, scale=3), comment="Amount of the financial information")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the financial information")
 
-    project_id = Column(ForeignKey("projects.id"), index=True)
+    project_id = Column(ForeignKey("projects.id"), index=True, comment="Foreign key referencing the associated project")
 
-    financetype_id = Column(ForeignKey("projectfinancetypes.id"), index=True)
+    financetype_id = Column(ForeignKey("projectfinancetypes.id"), index=True, comment="Foreign key referencing the financial information type")
     financetype = relationship("FinanceTypeModel", back_populates="finances")
 
-    created = Column(DateTime, server_default=sqlalchemy.sql.func.now())
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    created = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp when the financial information was created")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the financial information")
     createdby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
     changedby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
 
 
 class FinanceTypeModel(BaseModel):
+    """
+    Represents a type of financial information related to projects in the system.
+    """
     __tablename__ = "projectfinancetypes"
 
     id = UUIDColumn()
-    name = Column(String)
-    name_en = Column(String)
+    name = Column(String, comment="Name of the financial information type")
+    name_en = Column(String, comment="English name of the financial information type")
 
     finances = relationship("FinanceModel", back_populates="financetype")
 
-    created = Column(DateTime, server_default=sqlalchemy.sql.func.now())
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    created = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp when the financial information type was created")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the financial information type")
     createdby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
     changedby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
 
 class FinanceCategory(BaseModel):
+    """
+    Represents a category for financial information related to projects in the system.
+    """
     __tablename__ = "projectfinancecategories"
 
     id = UUIDColumn()
-    name = Column(String)
-    name_en = Column(String)
+    name = Column(String, comment="Name of the financial information category")
+    name_en = Column(String, comment="English name of the financial information category")
 
-    created = Column(DateTime, server_default=sqlalchemy.sql.func.now())
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    created = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp when the financial information category was created")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the financial information category")
     createdby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
     changedby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
 
 
 class MilestoneModel(BaseModel):
+    """
+    Represents a milestone for projects in the system.
+    """
     __tablename__ = "projectmilestones"
 
     id = UUIDColumn()
-    name = Column(String)
-    startdate = Column(DateTime)
-    enddate = Column(DateTime)
+    name = Column(String, comment="Name of the milestone")
+    startdate = Column(DateTime, comment="Start date of the milestone")
+    enddate = Column(DateTime, comment="End date of the milestone")
 
-    project_id = Column(ForeignKey("projects.id"), index=True)
+    project_id = Column(ForeignKey("projects.id"), index=True, comment="Foreign key referencing the associated project")
 
-    created = Column(DateTime, server_default=sqlalchemy.sql.func.now())
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    created = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp when the milestone was created")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the milestone")
     createdby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
     changedby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
 
 class MilestoneLinkModel(BaseModel):
+    """
+    Represents a link between milestones for projects in the system.
+    """
     __tablename__ = "projectmilestonelinks"
 
     id = UUIDColumn()
 
-    previous_id = Column(ForeignKey("projectmilestones.id"), index=True, nullable=True)
-    next_id = Column(ForeignKey("projectmilestones.id"), index=True, nullable=True)
+    previous_id = Column(ForeignKey("projectmilestones.id"), index=True, nullable=True, comment="Foreign key referencing the previous milestone")
+    next_id = Column(ForeignKey("projectmilestones.id"), index=True, nullable=True, comment="Foreign key referencing the next milestone")
 
-    created = Column(DateTime, server_default=sqlalchemy.sql.func.now())
-    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now())
+    created = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp when the milestone link was created")
+    lastchange = Column(DateTime, server_default=sqlalchemy.sql.func.now(), comment="Timestamp of the last change to the milestone link")
     createdby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
     changedby = UUIDFKey(nullable=True)#Column(ForeignKey("users.id"), index=True, nullable=True)
 
